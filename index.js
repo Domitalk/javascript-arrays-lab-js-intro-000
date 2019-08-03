9dade5e331c7f1987973ce1313b9ea2dd080c3f9
Dominic <dominic.m.chu@gmail.com>
2019-08-03 01:12:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  var kittens = kittens.slice(0, kittens.length - 1);
+  var kittens = kittens.slice(0, array.length - 1);
   return kittens;
 }
